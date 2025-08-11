9ede5da49fd641c25e5bd866d5f7d04fecd2e74a
Emanuel Coelho <emanuelcoelho1986@gmail.com>
2025-08-11 19:04:44 +0100

    fix: table product component test

--- tests/integration/components/products-table-test.ts
+++ tests/integration/components/products-table-test.ts
@@ -10,17 +10,31 @@ module('Integration | Component | products-table', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
+    this.set('columns', [
+      { name: 'Column 1' },
+      { name: 'Column 2' },
+      { name: 'Column 3' }
+    ]);
+
+    this.set('products', [
+      {
+        enrichedPropertyValues: [
+          { value: 'Value 1' },
+          { value: 'Value 2' },
+          { value: 'Value 3' }
+        ]
+      }
+    ]);
+
     await render(hbs`<ProductsTable />`);
 
     assert.dom().hasText('');
 
     // Template block usage:
     await render(hbs`
-      <ProductsTable>
-        template block text
-      </ProductsTable>
+      <ProductsTable />
     `);
 
-    assert.dom().hasText('template block text');
+    assert.dom().containsHtml("table");
   });
 });
